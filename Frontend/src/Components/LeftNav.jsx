7835bacddd41b8f0200750edcce34be81fe0c2b6
gopikant21 <gopikant5jan@gmail.com>
2024-10-29 18:56:04 +0530

    nav bar ready

--- Frontend/src/Components/LeftNav.jsx
+++ Frontend/src/Components/LeftNav.jsx
@@ -1,14 +1,12 @@
 import "./LeftNav.css"; // Make sure to create a corresponding CSS file for styling
 import homeIcon from "/home.svg";
 import contestsIcon from "/contests.svg";
-import menuIcon from "/menu.svg";
+import eventsIcon from "/events.svg";
+import progressIcon from "/progress.svg";
 
 const LeftNav = () => {
   return (
     <div className="left-nav-bar">
-      <ul>
-        <img src={menuIcon} />
-      </ul>
       <ul>
         <a href="/">
           <img src={homeIcon} />
@@ -20,8 +18,17 @@ const LeftNav = () => {
         </a>
       </ul>
       <ul>
-        <a href="/events">Events</a>
+        <a href="/rating">
+          <img src={progressIcon} />
+        </a>
       </ul>
+  
+      <ul>
+        <a href="/events">
+          <img src={eventsIcon} />
+        </a>
+      </ul>
+      
     </div>
   );
 };
--- Frontend/src/Components/TopNav.jsx
+++ Frontend/src/Components/TopNav.jsx
@@ -4,30 +4,38 @@ import notificationIcon from "/notification.svg";
 import profileIcon from "/profile.svg";
 import homeIcon from "/home.svg";
 import contestsIcon from "/contests.svg";
+import eventsIcon from "/events.svg";
+import progressIcon from "/progress.svg";
 
 const TopNav = () => {
   return (
     <nav className="top-nav">
       <div className="space"></div>
       <div className="right-side">
-      <div className="top-menu">
-            <ul>
-              <a href="/">
-                <img src={homeIcon} />
-              </a>
-            </ul>
-            <ul>
-              <a href="/contests">
-                <img src={contestsIcon} />
-              </a>
-            </ul>
-            <ul>
-              <a href="/events">Events</a>
-            </ul>
-          </div>
+        <div className="top-menu">
+          <ul>
+            <a href="/">
+              <img src={homeIcon} />
+            </a>
+          </ul>
+          <ul>
+            <a href="/contests">
+              <img src={contestsIcon} />
+            </a>
+          </ul>
+          <ul>
+            <a href="/rating">
+              <img src={progressIcon} />
+            </a>
+          </ul>
+          <ul>
+            <a href="/events">
+              <img src={eventsIcon} />
+            </a>
+          </ul>
+        </div>
         <div className="search-box">
           <input type="text" placeholder="Search" />
-          
         </div>
         <div className="profile-box">
           <a href="/" className="icon-button">
