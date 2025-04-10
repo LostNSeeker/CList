c5a0de1cc5266bbdcde0a21097413db57651ed49
Bipin <bk3284488@gmail.com>
2025-04-10 14:51:11 +0530

    Refactor firebaseAdmin.js to dynamically set service account path based on NODE_ENV

--- Backend/config/firebaseAdmin.js
+++ Backend/config/firebaseAdmin.js
@@ -2,7 +2,12 @@ const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./admin-key.json"); // Download from Firebase Console
+const serviceAccountPath =
+	process.env.NODE_ENV == "production"
+		? "/etc/secrets/admin-key.json"
+		: "./admin-key.json";
+
+const serviceAccount = require(serviceAccountPath); // Download from Firebase Console
 
 initializeApp({
 	credential: admin.credential.cert(serviceAccount),
